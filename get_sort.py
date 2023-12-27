86a42f2f6bd1eb24c5df2cabc136d947e095ea94
Humayun-Moon <humayunfreelancher01@gmail.com>
2023-12-28 00:09:50 +0600

    exception_handle

---
+++ get_sort.py
@@ -0,0 +1,22 @@
+# my_marks = [89, 87, 90,54, 87, 65]
+
+# def get_sort (number,n):
+#     number.sort()
+#     return number[-n:]
+# print(my_marks)
+# result = get_sort(my_marks, 1)
+# print(my_marks)
+
+# print(result)  
+
+
+# b = [i for i in range(0,2)]
+# print(b) 
+# my_list =[[y for y in range(1,10)] for i in range(0,3) ]
+# print(my_list)
+
+
+lst = [ i for i in range(1,1000) if i%2 != 0]
+print(lst) 
+
+
